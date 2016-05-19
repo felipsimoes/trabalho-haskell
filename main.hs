730d7e5366353227444ce16e65e764212b052de9
Felipe Simoes <felipe_lima_simoes@hotmail.com>
2016-05-19 00:26:03 +0000

    GET POST Usuario

--- main.hs
+++ main.hs
@@ -2,9 +2,52 @@
 {-# LANGUAGE QuasiQuotes           #-}
 {-# LANGUAGE TemplateHaskell       #-}
 {-# LANGUAGE TypeFamilies          #-}
-import           Yesod
+import Yesod
+import Database.Persist.Postgresql
+import Data.Text
+import Control.Monad.Logger(runStdoutLoggingT)
 
-data HelloWorld = HelloWorld
+data Pagina = Pagina{connPool :: ConnectionPool}
+
+instance Yesod Pagina
+
+share [mkPersist sqlSettings, mkMigrate "migrateAll"] [persistLowerCase|
+
+Usuario json
+    nome Text
+    senha Text
+    deriving Show
+    
+|]
+
+mkYesod "Pagina" [parseRoutes|
+
+/cadastro/usuario UsuarioR GET POST
+--/cadastro/pessoa GET POST
+|]
+
+instance YesodPersist Pagina where
+    type YesodPersistBackend Pagina = SqlBackend
+    runDB f = do 
+        master <- getYesod
+        let pool = connPool master
+        runSqlPool f pool
+        
+--FUNÇOES
+
+getUsuarioR :: Handler ()
+getUsuarioR = do
+    allUsuario <- runDB $ selectList [] [Asc UsuarioNome ]
+    sendResponse (object [pack "data" .= fmap toJSON allUsuario])
+
+postUsuarioR :: Handler ()
+postUsuarioR = do
+    usuario <- requireJsonBody :: Handler Usuario
+    runDB $ insert usuario
+    sendResponse (object [pack "resp" .= pack "Criado"])
+
+
+{- data HelloWorld = HelloWorld
 
 mkYesod "HelloWorld" [parseRoutes|
 / HomeR GET
@@ -13,7 +56,18 @@ mkYesod "HelloWorld" [parseRoutes|
 instance Yesod HelloWorld
 
 getHomeR :: Handler Html
-getHomeR = defaultLayout [whamlet|Hello World!|]
+getHomeR = defaultLayout [whamlet|Hello World!|] -}
+
+
+
+connStr = "dbname=d4htbg71jrvj1f host=ec2-107-20-174-127.compute-1.amazonaws.com user=kcepfkqlcfbgpx password=ypVq9Yx6t4Q1InDMvoT-yR7Idk port=5432"
 
 main :: IO ()
-main = warp 8080 HelloWorld
+main = runStdoutLoggingT $ withPostgresqlPool connStr 10 $ \pool -> liftIO $ do
+runSqlPersistMPool (runMigration migrateAll) pool
+warp 8080 (Pagina pool)
+
+
+
+
+
